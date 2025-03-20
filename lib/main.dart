ba7ede8f2fad6c28178f964535d3af5d717df1f5
GABRIEL_ALEJANDRO_MONTES_HERNANDEZ <a.22308051281276@cbtis128.edu.mx>
2025-03-20 18:56:10 +0000

    b

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Gabriel Montes',
+            style: const TextStyle(
+                fontStyle: FontStyle.italic, color: Colors.white),
+          ),
+          backgroundColor: const Color(0xffb70e02),
+        ),
+        body: ClientesCard(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class ClientesCard extends StatelessWidget {
+  // Diccionario de ejemplo con información de clientes
+  final Map<String, String> clientes = {
+    'nombre': 'Gabriel Montes',
+    'email': 'gabmon@email.com',
+    'telefono': '123-456-7890',
+    'direccion': 'Calle luna',
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return Center(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15), // Bordes redondeados
+        ),
+        elevation: 10, // Sombra
+        color: Colors.blue[100], // Color de fondo de la tarjeta
+        margin: const EdgeInsets.all(15), // Márgenes alrededor de la tarjeta
+        child: Padding(
+          padding: const EdgeInsets.all(20), // Menor espaciado interno
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Ajustar al tamaño del contenido
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Nombre: ${clientes['nombre']}',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4), // Menor espacio entre textos
+              Text(
+                'Email: ${clientes['email']}',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 4), // Menor espacio entre textos
+              Text(
+                'Teléfono: ${clientes['telefono']}',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 4), // Menor espacio entre textos
+              Text(
+                'Direccion: ${clientes['direccion']}',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
